06d5f2595941e95f4a380536008d5c7da5d92142
samir-s-hassan <hsamir347@gmail.com>
2024-03-11 22:30:44 -0400

    finished app by adding a gradient layer

--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -38,6 +38,7 @@ class ForecastViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addGradient()
         forecasts = createMockData()
         configure(with: forecasts[selectedForecastIndex]) // configure the UI to show the first mock data
     }
@@ -77,6 +78,16 @@ class ForecastViewController: UIViewController {
       let dateFormatter = DateFormatter()
       dateFormatter.dateFormat = "MMMM d, yyyy"
       dateLabel.text = dateFormatter.string(from: forecast.date)
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor(red: 0.54, green: 0.88, blue: 0.99, alpha: 1.00).cgColor,
+                                UIColor(red: 0.51, green: 0.81, blue: 0.97, alpha: 1.00).cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
     
