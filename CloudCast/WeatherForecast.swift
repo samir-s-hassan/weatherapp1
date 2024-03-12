bcbbba6b60c95cee7b83ced79e11c7e24e6de7cd
samir-s-hassan <hsamir347@gmail.com>
2024-03-11 22:11:26 -0400

    can now dynamically display properties of weatherforecast

---
+++ CloudCast/WeatherForecast.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherForecast.swift
+//  CloudCast
+//
+//  Created by Samir Hassan on 3/11/24.
+//
+
+import Foundation
+import UIKit
+
+struct WeatherForecast {
+  let temperature: Double
+  let date: Date
+}
+
