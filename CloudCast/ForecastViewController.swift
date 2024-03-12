a96e5c2ee3cc3e714092c615812583eb191e488b
samir-s-hassan <hsamir347@gmail.com>
2024-03-11 20:31:18 -0400

    starting basic weather app project

---
+++ CloudCast/ForecastViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Samir Hassan on 3/11/24.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
